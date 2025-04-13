5ef6fe36e1ea116959e5ad64ee999c2693d994ab
RajKKapadia <raajforyou@gmail.com>
2025-04-13 16:59:07 +0530

    reading port from .env

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 if __name__ == "__main__":
     import uvicorn
+    from src import config
 
-    port = 5000
-
-    uvicorn.run(app="src.main:app", port=port, reload=True)
+    uvicorn.run(app="src.main:app", port=config.PORT, reload=True)
